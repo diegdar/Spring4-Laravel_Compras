62ca828f883f849387935a8d4538161b82f9fef9
Diego Chacon <diegdarch@hotmail.com>
2024-03-27 16:55:07 +0100

    1:Created view=>home.blade.php 2:Created HomeController 3: Created home route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::resource('products', ProductController::class);
+Route::resource('purchases', PurchaseController::class);
+Route::resource('productPurchases', ProductPurchaseController::class);
